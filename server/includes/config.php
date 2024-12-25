32789fac91ef499b675cf1b88b1375801a4be77a
svxezm <igorb.kuhl@gmail.com>
2024-12-25 15:34:22 -0300

    feat: define path to database file

--- server/includes/config.php
+++ server/includes/config.php
@@ -3,4 +3,5 @@ define('BASE_URL', 'http://localhost');
 define('PUBLIC_URL', BASE_URL . '/../../client/public');
 define('STYLES_URL', PUBLIC_URL . '/styles');
 define('SCRIPTS_URL', PUBLIC_URL . '/scripts');
+define('DB_FILE', __DIR__ . '/../db/database.db');
 ?>
