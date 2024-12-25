a6b1a4e3f4b8bc9e623671c9e50dce30b210199f
svxezm <igorb.kuhl@gmail.com>
2024-12-25 15:31:45 -0300

    fix: specify POST value names to prevent future conflicts

--- server/enter.php
+++ server/enter.php
@@ -3,15 +3,15 @@ require_once 'includes/db.php';
 $db = connectDb();
 
 $query = $db->prepare('SELECT name, password FROM users WHERE name = :name');
-$query->bindValue(':name', $_POST['username'] ?? '');
+$query->bindValue(':name', $_POST['login-name'] ?? '');
 $result = $query->execute();
 
 $userData = $result->fetchArray(SQLITE3_ASSOC);
 $storedName = $userData['name'] ?? '';
 $storedPassword = $userData['password'] ?? '';
 
-$inputName = $_POST['username'] ?? '';
-$inputPassword = $_POST['password'] ?? '';
+$inputName = $_POST['login-name'] ?? '';
+$inputPassword = $_POST['login-password'] ?? '';
 
 if ($inputName === $storedName && password_verify($inputPassword, $storedPassword)) {
     header('Location: pages/user_list.php');
