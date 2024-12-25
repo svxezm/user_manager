cecb7f693d9b33106a1fee57b11ec69bd0f2130f
svxezm <igorb.kuhl@gmail.com>
2024-12-24 21:50:22 -0300

    chore: move public at root directory to client

---
+++ client/src/scripts/user_list.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $(".users__title").on("click", function() {
+        $(".users__content").slideToggle();
+    })
+})
--- public/scripts/user_list.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(() => {
-    $(".users__title").on("click", () => {
-        $(".users__content").slideToggle();
-    })
-})
