66a057015f4f733f59e859f278c0ced8e1139c62
svxezm <igorb.kuhl@gmail.com>
2024-12-24 22:05:22 -0300

    chore: update public URL

--- server/includes/config.php
+++ server/includes/config.php
@@ -1,6 +1,6 @@
 <?php
 define('BASE_URL', 'http://localhost');
-define('PUBLIC_URL', BASE_URL . '/public');
+define('PUBLIC_URL', BASE_URL . '/../../client/public');
 define('STYLES_URL', PUBLIC_URL . '/styles');
 define('SCRIPTS_URL', PUBLIC_URL . '/scripts');
 ?>
