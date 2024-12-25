74512bf31e4293800b599850418e364b196e4350
svxezm <igorb.kuhl@gmail.com>
2024-12-24 21:52:13 -0300

    feat: set SQL script to create a new initial table

---
+++ server/migrations/create_users_table.sql
@@ -0,0 +1,7 @@
+-- migrations/create_users_table.sql
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    password TEXT NOT NULL,
+    picture TEXT NOT NULL
+);
